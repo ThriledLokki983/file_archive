e1b22bd28b4f73fd4f57b5056d860dd3bf03b679
ThriledLokki983 <gnimoh001@gmail.com>
2021-11-13 19:22:45 +0100

    file archive initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className="App">
+			<h1>file archive ready to develop</h1>
+		</div>
+	);
+}
+
+export default App;
